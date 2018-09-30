4c7682db2e61a39cb1f5adf130cc499f388cd3f5
springdeer <15075123292@163.com>
2018-09-30 13:31:25 +0800

    增加简单注释.

--- parker/dbaccess.h
+++ parker/dbaccess.h
@@ -36,21 +36,51 @@ int db_query_credits(mysqlquery_t dbinst, int* cnt, int* credits);
 int db_query_travelername(mysqlquery_t dbinst, int cardid, char **name);
 
 //////////////////////////////////////////////////////////////////////////
+/*
+*查询未处理订单
+*/
 int db_query_unsettled_orders(mysqlquery_t dbinst, char* where,unsettled_order_t ** outorders,int * count);
+
+/*
+*增加未处理订单
+*/
 int db_save_unsettled_orders(mysqlquery_t dbinst,unsettled_order_t orders,int count);
 
-//username == NULL 则删除所有orderno的订单
-//username != NULL 则删除符合uername条件的订单项目
+/*
+*删除未处理订单
+*username == NULL时 则删除所有orderno的订单
+*username != NULL时  则删除符合uername条件的订单项目
+*/
 int db_delete_unsettled_orders(mysqlquery_t dbinst, char* orderno, char* username);
+
+/*
+*更改未处理订单
+*/
 int db_modify_unsettled_orders(mysqlquery_t dbinst, char* where, char* set);
 
 //////////////////////////////////////////////////////////////////////////
+/*
+*更改已处理订单
+*/
 int db_query_settled_orders(mysqlquery_t dbinst, char* where, settled_order_t ** outorders, int * count);
+
+/*
+*新增已处理订单
+*/
 int db_save_settled_orders(mysqlquery_t dbinst, settled_order_t orders, int count);
 
-//username == NULL 则删除所有orderno的订单
-//username != NULL 则删除符合uername条件的订单项目
+/*
+*删除已处理订单
+*username == NULL时 则删除所有orderno的订单
+*username != NULL时  则删除符合uername条件的订单项目
+*/
 int db_delete_settled_orders(mysqlquery_t dbinst, char* orderno, char* username);
+
+/*
+*更改已处理订单
+*username == NULL时 则删除所有orderno的订单
+*username != NULL时  则删除符合uername条件的订单项目
+*/
 int db_modify_settled_orders(mysqlquery_t dbinst, char* where, char* set);
 
 #endif
