301b06ba92a50224d7b8463e301773bc548cb5a2
springdeer <15075123292@163.com>
2018-10-01 17:19:18 +0800

    增加注释.

--- parker/datatransfers.h
+++ parker/datatransfers.h
@@ -3,8 +3,9 @@
 
 #include "datatransfer.h"
 
+//莫有使用config，后续可扩展使用.
 void       transfers_startup(config_t config);
-int        transfers_pushAjob(job_st job);
+int        transfers_pushAjob(job_st  job);
 int        transfers_stop();
 int        transfers_free();
 
